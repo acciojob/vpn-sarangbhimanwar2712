9dd8e5e9e827b3dbaeb4aa7b3bd52ee63ae44b17
Sarang <sarangbhimanwar5@gmail.com>
2023-03-04 23:06:38 +0530

    LastCommit

--- src/main/java/com/driver/repository/CountryRepository.java
+++ src/main/java/com/driver/repository/CountryRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 
-    public Country findByName(String name) ;
 }
